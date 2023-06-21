855947eac489cf1bc54346c2033e6cee868b0441
Alexander Nazimov <s-nazim@list.ru>
2023-06-21 08:24:14 +0300

    add m7

--- Module7/main5.cpp
+++ Module7/main5.cpp
@@ -3,12 +3,20 @@ using namespace std;
 
 int main()
 {
+    // int raws{};
+    // cin >> raws;
+    // int cols{};
+    // cin >> cols;
+
     for(int i = 0; i < 15; ++i)
     {
         if (i == 7)
         {
-            for(int j = 0; j < 15; ++j)
+            for(int j = 0; j < 15; ++j){
                 cout << (j == 7 ? '+' : '-');
+                if(j == 14)
+                    cout << ">";
+            }
             
         }
         else if(i == 0)
