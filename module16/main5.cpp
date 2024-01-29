fdc2054b128a0cb8609a3d530d573ab9053f89ac
NAZIM_BATYA <nazim>
2024-01-29 22:10:47 +0300

    add main5.cpp module16

--- module16/main5.cpp
+++ module16/main5.cpp
@@ -2,47 +2,81 @@
 #include <string>
 #include <sstream>
 using namespace std;
-
+int gKalvin{5000};
 enum switches 
 { 
-    LIGHTS_INSIDE = 1, 
-    LIGHTS_OUTSIDE = 2, 
-    HEATERS = 4, 
-    WATER_PIPE_HEATING = 8, 
-    CONDITIONER = 16 
+    POWER_HOUSE = 1,
+    ROZETKI_HOUSE =2,
+    LIGHTS_INSIDE = 4, 
+    LIGHTS_OUTSIDE = 8, 
+    OTOPLENIE_INSIDE = 16, 
+    OTOPLENIE_TRUB = 32, 
+    CONDITIONER = 64 
 };
 
-void parseData(string& str){
+void parseData(string& str, int& hours, int& switches_state){
     stringstream buffer_str(str);
     int temperature_inside, temperature_outside;
     string movement, lights;
     buffer_str >> temperature_inside >> temperature_outside >> movement >> lights ;
 
-    // Логика умного дома
-    if (temperature_outside < 0) {
-        // Включить систему обогрева водопровода
-        std::cout << "Water pipe heating ON!" << std::endl;
-    } else if (temperature_outside > 5) {
-        // Выключить систему обогрева водопровода
-        std::cout << "Water pipe heating OFF!" << std::endl;
+   
+    if(temperature_outside < 0 && !(switches_state & OTOPLENIE_TRUB)) {
+        cout << "Нагрев труб включен " << endl;
+        switches_state |= OTOPLENIE_TRUB;
+    }
+    else if(temperature_outside > 5 && (switches_state & OTOPLENIE_TRUB)) {
+        cout << "Нагрев труб выключен " << endl;
+        switches_state ^= OTOPLENIE_TRUB;  
     }
 
-    if (lights == "on" && movement == "yes" ) {
-        // Включить садовое освещение
-        std::cout << "Garden lights ON!" << std::endl;
-    } else {
-        // Выключить садовое освещение
-        std::cout << "Garden lights OFF!" << std::endl;
+    if(hours > 16 && hours < 5 && movement == "yes" && !(switches_state & LIGHTS_OUTSIDE)) {
+        cout << "Садовое освещение включено!" << endl;
+        switches_state |= LIGHTS_OUTSIDE;
+    } 
+    else if(switches_state & LIGHTS_OUTSIDE){
+        cout << "Садовое освещение выключено!" << std::endl;
+        switches_state ^= LIGHTS_OUTSIDE;
     }
 
+    if(temperature_inside < 22 && !(switches_state & OTOPLENIE_INSIDE)){
+        cout << "Отопление внутри включено!" << endl;
+        switches_state |= OTOPLENIE_INSIDE;
+    }
+    else if(temperature_inside >= 25 && switches_state & OTOPLENIE_INSIDE){
+        cout << "Отопление внутри выключено!" << endl;
+        switches_state ^= OTOPLENIE_INSIDE;
+    }
+
+    if(temperature_inside >= 30 && !(switches_state & CONDITIONER)){
+        cout << "Кондер включен!" << endl;
+        switches_state |= CONDITIONER;
+    }
+    else if(temperature_inside <= 25 && switches_state & CONDITIONER){
+        cout << "Кондер выключен!" << endl;
+        switches_state ^= CONDITIONER;
+    }
+
+    if(hours >= 16 && hours <= 20 && lights == "on"){
+        gKalvin -= 300;
+        cout << "Color temperature:" << gKalvin << "K" << endl;
+    }
+    else if(hours == 0 && lights == "on"){
+        cout << "Lights ON!" << endl; 
+        cout << "Color temperature:" << gKalvin << "K" << endl;
+        gKalvin = 5000;
+    }
 }
 
 
 int main(){
-    for(int i = 0; i < 48; i++){
-        string str;
-        getline(cin, str);
-        parseData(str);
+    for(int days = 0; days < 2; days++){
+        for(int hours = 0; hours <= 23; hours++){
+            int switches_state{1};
+            string str;
+            cout << "Temperature inside, temperature outside, movement, lights: \n"; 
+            getline(cin, str);
+            parseData(str, hours, switches_state);
+        }
     }
-
 }
