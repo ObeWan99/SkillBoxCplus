8e12265aa31626733a2fc8fb7bbd9b0cd4a7c7c8
батя <s-nazim>
2024-03-29 17:17:58 +0300

    add mod 24

--- module24/main2.cpp
+++ module24/main2.cpp
@@ -31,18 +31,32 @@ int main() {
     std::time_t now = std::time(nullptr);
     std::tm* currentTime = std::localtime(&now);
     
+    std::tm closestBirthday = {0};
+    bool foundClosest = false;
+    
     for (const auto& friendData : friends) {
         if (friendData.birthday.tm_mon < currentTime->tm_mon || 
             (friendData.birthday.tm_mon == currentTime->tm_mon && friendData.birthday.tm_mday < currentTime->tm_mday)) {
             continue; // Skip if birthday has already passed this year
         }
-        
-        if (friendData.birthday.tm_mon == currentTime->tm_mon && friendData.birthday.tm_mday == currentTime->tm_mday) {
-            std::cout << "Happy Birthday, " << friendData.name << "!\n";
-        } else {
-            std::cout << "Next birthday for " << friendData.name << " is on: " 
-                      << friendData.birthday.tm_mon + 1 << "/" << friendData.birthday.tm_mday << std::endl;
+        std::tm tempFriendBirthday = friendData.birthday;
+        std::tm tempClosestBirthday = closestBirthday;
+
+        if (!foundClosest || difftime(std::mktime(&tempFriendBirthday), std::mktime(&tempClosestBirthday)) < 0) {
+            closestBirthday = friendData.birthday;
+            foundClosest = true;
         }
+    }
+    
+    if (foundClosest) {
+        if (closestBirthday.tm_mon == currentTime->tm_mon && closestBirthday.tm_mday == currentTime->tm_mday) {
+            std::cout << "Happy Birthday today, " << friends[0].name << "!\n";
+        } else {
+            std::cout << "Next closest birthday is for " << friends[0].name << " on: " 
+                      << closestBirthday.tm_mon + 1 << "/" << closestBirthday.tm_mday << std::endl;
+        }
+    } else {
+        std::cout << "No upcoming birthdays found." << std::endl;
     }
     
     return 0;
