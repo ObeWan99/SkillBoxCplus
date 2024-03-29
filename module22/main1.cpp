6aba0fffd29d774d10d2f1a0d169e25fa6426344
батя <s-nazim>
2024-03-29 09:24:17 +0300

    add mod22

--- module22/main1.cpp
+++ module22/main1.cpp
@@ -1,47 +1,46 @@
 #include <iostream>
-#include <map>
 #include <string>
+#include <map>
+#include <vector>
 
-int main(){
-    std::map<std::string, std::string> telephone_directory;
-    std::string name;
-    std::string number;
-    int cmd{};
-
-    while (cmd != 4)
-    {
-        std::cout << "Нажмите 1 для добавления нового контакта " << std::endl <<
-        "Нажмите 2, чтобы узнать фамилию абонента по номеру телефона " << std::endl <<
-        "Нажмите 3, чтобы узнать телефон абонента по фамилии " << std::endl <<
-        "Нажмите 4 для выхода " << std::endl;
-        
-        std::cin >> cmd;
-
-        if(cmd == 1){
-            std::cout << "Введите номер абонента и фамилию: ";
-            std::cin >> number >> name;
-            telephone_directory.insert(std::make_pair(number, name));
-        }
-        else if(cmd == 2){
-            std::cout << "Введите номер абонента: ";
-            std::cin >> number;
-            std::map<std::string, std::string>::iterator it = telephone_directory.find(number);
-            if(it != telephone_directory.end()){
-                std::cout << "Имя: " << it->second << std::endl;
-            } else{
-                std::cout << "Имя не найдено по номеру" << std::endl;
-            }
-        }
-        else if(cmd == 3){
-            std::cout << "Введите имя абонента: ";
-            std::cin >> name;
-            for(std::map<std::string, std::string>::iterator it = telephone_directory.begin(); it != telephone_directory.end(); it++){
-                if(it->second == name){
-                    std::cout << "Номер телефона: " << it->first << std::endl;
-                }
-            }
-
-        }  
-    }     
+// Функция для добавления абонента в справочник
+void addContact(std::map<std::string, std::string>& numberToSurname, std::map<std::string, std::vector<std::string>>& surnameToNumber, const std::string& phoneNumber, const std::string& lastName) {
+    // создаём пару телефон : имя
+    numberToSurname[phoneNumber] = lastName;
+    surnameToNumber[lastName].push_back(phoneNumber);
 }
 
+// Функция для поиска фамилии по номеру телефона
+std::string findLastNameByNumber(const std::map<std::string, std::string>& numberToSurname, const std::string& phoneNumber) {
+    if (numberToSurname.count(phoneNumber) > 0) {
+        return numberToSurname.at(phoneNumber);
+    }
+    return "Фамилия не найдена";
+}
+
+// Функция для поиска номера(ов) по фамилии
+std::string findNumbersByLastName(const std::map<std::string, std::vector<std::string>>& surnameToNumber, const std::string& lastName) {
+    if (surnameToNumber.count(lastName) > 0) {
+        std::string numbers;
+        for (const auto& number : surnameToNumber.at(lastName)) {
+            numbers += number + " ";
+        }
+        return numbers;
+    }
+    return "Номера не найдены";
+}
+
+int main() {
+    std::map<std::string, std::string> numberToSurname;
+    std::map<std::string, std::vector<std::string>> surnameToNumber;
+
+    // Пример запросов
+    addContact(numberToSurname, surnameToNumber, "69-70-30", "Ivanov");
+    addContact(numberToSurname, surnameToNumber, "69-70-31", "Petrov");
+    addContact(numberToSurname, surnameToNumber, "69-70-30", "Ivanov");
+
+    std::cout << findLastNameByNumber(numberToSurname, "69-70-30") << std::endl; // Выводит "Ivanov"
+    std::cout << findNumbersByLastName(surnameToNumber, "Ivanov") << std::endl; // Выводит "69-70-30 "
+
+    return 0;
+}
